011a7a5f25e11b0e0473f6c4eb65a06f93ca7168
Joey <145919209+Swifty433@users.noreply.github.com>
2025-08-11 19:55:52 +0100

    🚧 Setting Up Unit Tests

---
+++ Dsa Repeat Exam Aug Test/OrderedArrayTest.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\OrderedArray.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace OrderedArrayTest
+{
+	TEST_CLASS(OrderedArrayTest)
+	{
+	public:
+		
+        TEST_METHOD(TestPushMaintainsNaturalOrder)
+        {
+            
+        }
+	};
+}
--- MainApp.cpp
+++ MainApp.cpp
@@ -5,6 +5,21 @@
 int main() {
 	OrderedArray<int> arr;
 
-	arr.push(5);
-	arr.push(10);
+    arr.push(5);
+    arr.push(2);
+    arr.push(8);
+    arr.push(1);
+    arr.push(3);
+
+    std::cout << "Initial contents: ";
+    arr.print();
+
+    //show the length of array
+    std::cout << "Length: " << arr.length() << "\n"; //returns the length of array
+
+    //get the element at index
+    std::cout << "Element at index 2: " << arr.getElement(2) << "\n"; //returns the element at specified index
+
+    //get and element at an invalid index
+    std::cout << "Element at index 10: " << arr.getElement(10) << "\n";  //returns 0 if no elements are found
 }
