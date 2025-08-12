3d84c83e9cc6810c66d43def59f05950f3988f7d
Joey <145919209+Swifty433@users.noreply.github.com>
2025-08-12 21:53:02 +0100

    ✨ Created some unit tests, Will come back and do more

--- Dsa Repeat Exam Aug Test/OrderedArrayTest.cpp
+++ Dsa Repeat Exam Aug Test/OrderedArrayTest.cpp
@@ -13,7 +13,68 @@ namespace OrderedArrayTest
 		
         TEST_METHOD(TestPushMaintainsNaturalOrder)
         {
-            
+			OrderedArray<int> arr;
+			arr.push(5);
+			arr.push(3);
+			arr.push(8);
+			arr.push(1);
+
+			Assert::AreEqual(4, arr.length());
+			Assert::AreEqual(1, arr.getElement(0));
+			Assert::AreEqual(3, arr.getElement(1));
+			Assert::AreEqual(5, arr.getElement(2));
+			Assert::AreEqual(8, arr.getElement(3));
         }
+
+		TEST_METHOD(TestRemoveElement)
+		{
+			OrderedArray<int> arr;
+			arr.push(5);
+			arr.push(3);
+			arr.push(8);
+
+			Assert::AreEqual(true, arr.remove(1));
+			Assert::AreEqual(2, arr.length());
+		}
+
+		TEST_METHOD(TestRemoveInvalidIndex)
+		{
+			OrderedArray<int> arr;
+			arr.push(5);
+			arr.push(3);
+			arr.push(8);
+			Assert::AreEqual(false, arr.remove(5)); 
+			Assert::AreEqual(3, arr.length()); 
+		}
+
+		TEST_METHOD(TestSearchForElement)
+		{
+			OrderedArray<int> arr;
+			arr.push(5);
+			arr.push(3);
+			arr.push(8);
+
+			Assert::AreEqual(1, arr.search(5));
+		}
+
+		TEST_METHOD(TestSearchForNonElement)
+		{
+			OrderedArray<int> arr;
+			arr.push(5);
+			arr.push(3);
+			arr.push(8);
+
+			Assert::AreEqual(-1, arr.search(10));
+		}
+
+		TEST_METHOD(TestClear)
+		{
+			OrderedArray<int> arr;
+			arr.push(5);
+			arr.push(3);
+			arr.push(8);
+			arr.clear();
+			Assert::AreEqual(0, arr.length());
+		}
 	};
 }
