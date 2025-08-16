378baa74f41501f1575d4b74f2a207cf3f81c6c5
Joey <145919209+Swifty433@users.noreply.github.com>
2025-08-16 21:44:31 +0100

    ✨ Created the CSV reader files
    starting implementation

---
+++ CSV.cpp
@@ -0,0 +1,28 @@
+#include "CSV.h"
+
+Lead CSV::parseCSV(const std::string& line, const std::string& repName) const
+{
+	std::string name, email, companyName;
+	int phoneNumber;
+	std::stringstream ss(line);
+
+	//CSV parsing logic
+	std::getline(ss, name, ',');
+	std::getline(ss, email, ',');
+	ss >> phoneNumber;
+	std::getline(ss, companyName, ',');
+
+	return Lead(name, email, phoneNumber, companyName, repName);
+}
+
+MySet<Lead> CSV::readCSV(const std::string& filename, const std::string& repName)
+{
+	MySet<Lead> leads;
+	std::ifstream file(filename);
+
+	if(!file.is_open())
+	{
+		std::cout << ("Couldnt open the file: " + filename);
+		return leads;
+	}
+}
