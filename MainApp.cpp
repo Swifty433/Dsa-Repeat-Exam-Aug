8e1bf9ab864e3dd3e7defd5c1dab703d3702b554
Joey <145919209+Swifty433@users.noreply.github.com>
2025-08-19 02:25:39 +0100

    ✨ Finished Main App and Part 3
    Just need to polish and add more comments

--- MainApp.cpp
+++ MainApp.cpp
@@ -22,4 +22,12 @@ int main() {
 	//Reading CSV files
 	MySet<Lead> leads1 = processor.readCSV(file1, rep1);
 	MySet<Lead> leads2 = processor.readCSV(file2, rep2);
+
+	MySet<Lead> CommonLeads = leads1 & leads2; // Intersection
+
+	MySet<Lead> UniqueLeads = leads1 | leads2; // Union
+
+	//Write them to a file
+	processor.writeCSV("CommonLeads.csv", CommonLeads);
+	processor.writeCSV("UniqueLeads.csv", UniqueLeads);
 }
